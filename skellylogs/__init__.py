528fd2ec34cfc9d7e6a13a1e8883e3fc0e9bccc6
jonmatthis <jonmatthis@gmail.com>
2026-02-20 08:40:22 -0500

    add to __all__

--- skellylogs/__init__.py
+++ skellylogs/__init__.py
@@ -1,9 +1,11 @@
 from skellylogs.configure_logging import configure_logging
 from skellylogs.log_levels import LogLevels
-from skellylogs.handlers.websocket_log_queue_handler import LogRecordModel
+from skellylogs.handlers.websocket_log_queue_handler import LogRecordModel, get_websocket_log_queue, create_websocket_log_queue
 
 __all__ = [
     "configure_logging",
     "LogLevels",
     "LogRecordModel",
+    "get_websocket_log_queue",
+    "create_websocket_log_queue",
 ]
